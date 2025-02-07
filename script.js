c0d4cddde41940e61c0924bd8f89851030ae3c21
ghxyst <anaghskumar2000@gmail.com>
2025-02-07 17:22:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sherikkum??",
+    "Onnude aalochich Nokk?",
+    "Meenuu Funde please...",
+    "Aaloochik Rn Fast!",
+    "No paranjal I'll cry...",
+    "Njan karayum...",
+    "Njan ippol karayum!...",
+    "Ooh!! Njan choikun illa!...",
+    "Mariyadekku Yes paranjo! ❤️"
 ];
 
 let messageIndex = 0;
